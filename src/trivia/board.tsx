18eef360807b04d766f6a2272d4661d67f13f169
Josh Dunsmoor <joshd@northpointgis.com>
2021-11-16 23:54:02 -0600

    Add button to make the api call for questions

--- src/trivia/board.tsx
+++ src/trivia/board.tsx
@@ -1,29 +1,50 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import QuestionTile from './question';
+import Shuffle from './shuffle';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 
-type BoardProps = {
+
+type boardState = {
+    gameComplete: boolean
+}
+
+type boardProps = {
     questions: []
 }
 
 
-const TriviaBoard: React.FC<BoardProps> = () => {
-    return (
-        <Container fluid className="trivia-board">
-            <Row className="justify-content-center">
-                <QuestionTile />
-                <QuestionTile />
-                <QuestionTile />
-            </Row>
-            <Row className="justify-content-center">
-                <QuestionTile />
-                <QuestionTile />
-            </Row>
-        </Container>
-    )
-}
+class TriviaBoard extends React.Component<boardProps, boardState> {
+    constructor(props: boardProps) {
+        super(props);
+        this.handleShuffle = this.handleShuffle.bind(this);
+    }
 
+    state: boardState = {
+        gameComplete: false
+    }
+
+    handleShuffle() {
+        console.log('this will make request from api')
+    }
+
+    render() {
+        return (
+            <Container fluid className="trivia-board">
+                <Shuffle text="Get Some Trivia" handleShuffle={this.handleShuffle} />
+                <Row className="justify-content-center">
+                    <QuestionTile />
+                    <QuestionTile />
+                    <QuestionTile />
+                </Row>
+                <Row className="justify-content-center">
+                    <QuestionTile />
+                    <QuestionTile />
+                </Row>
+            </Container>
+        )
+    }
+}
 
 export default TriviaBoard;
